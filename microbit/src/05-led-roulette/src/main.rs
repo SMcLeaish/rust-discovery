a36d3791386cb6751aec6effceefd8dd7772251a
Sean McLeaish <smcleaish@gmail.com>
2024-11-28 16:17:32 -0500

    5.4 light an led

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,15 @@
 #![no_std]
 
 use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
 use panic_halt as _;
-use microbit as _;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let mut board = Board::take().unwrap();
+    board.display_pins.col2.set_low().unwrap();
+    board.display_pins.row2.set_high().unwrap();
 
-    // infinite loop; just so we don't leave this stack frame
     loop {}
 }
