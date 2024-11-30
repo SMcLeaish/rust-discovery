21250711aaa7a2834e09efdc84a69799fea32883
Sean McLeaish <smcleaish@gmail.com>
2024-11-30 08:59:18 -0500

    5.6 using a match on row to define row specific behavior

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -17,15 +17,43 @@ fn main() -> ! {
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = microbit::display::blocking::Display::new(board.display_pins);
-    let light_it_all = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
+    let mut led_state = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
     loop {
-        display.show(&mut timer, light_it_all, 1000);
+        for row_idx in 0..led_state.len() {
+            for col_idx in 0..led_state[row_idx].len() {
+                let value = &mut led_state[row_idx][col_idx];
+                let previous_value = if col_idx > 0 {
+                    Some(&mut led_state[row_idx][col_idx - 1])
+                } else {
+                    None
+                };
+                match row_idx {
+                    0 => {
+                        if *previous_value == 1 {
+                            *previous_value = 0;
+                        }
+                        if *value == 1 {
+                            *value = 0;
+                        } else {
+                            *value = 1;
+                        }
+                        rprintln!("Set led_state[{}][{}] to {}", row_idx, col_idx, *value);
+                        timer.delay_ms(300_u16);
+                    }
+                    _ => {
+                        rprintln!("Value at led_state[{}][{}] is {}", row_idx, col_idx, *value);
+                        timer.delay_ms(300_u16);
+                    }
+                }
+            }
+        }
+
         display.clear();
         timer.delay_ms(1000_u16);
     }
